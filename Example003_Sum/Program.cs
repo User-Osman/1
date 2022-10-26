447811eebbff3093992240824e80e5ec852c5bfd
Osman <osman-z-1995@mail.ru>
2022-10-26 15:04:19 +0300

    and good

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,24 @@
-﻿int numberA = 3;
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿/// <summary>
+/// Ввод-вывод с консоли квадратной матрицы
+/// </summary>
+public void InOutMatrix();
+{
+    int n;
+    Console.WriteLine("Ввод квадратной матрицы A размерности n");
+    Console.WriteLine("Введите целое - n");
+    n = Convert.ToInt32(Console.ReadLine());
+    double[,] A = new double[n,n]; 
+    for(int i = 0; i<n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            Console.WriteLine("Введите A[{0}],[{1}]", i, j);
+            A[i, j] = Convert.ToDouble(Console.ReadLine());
+        }
+    Console.WriteLine("Вывод матрицы A");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)                
+            Console.Write("A[{0}],[{1}] = {2}  ", i, j, A[i, j]);
+        Console.WriteLine();
+    }
+}
